4a12ef90f143c1c6e9c00697d73d947d275b2cbe
elm-dak <mestafa.wydad66@gmail.com>
2024-02-17 23:48:09 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{'textAlign': 'center', "fontFamily": "'Open Sans', sans-serif", 'fontSize': '16px'}}>
+    <h2>Project for test DevOPs Dakouky</h2>
+    </div>
+  );
+}
+
+export default App;
